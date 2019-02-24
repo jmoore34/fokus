d6f142ac3ec10a08e9b1f9a2322dc5d413eca232
jchillin <cooljacobhill@gmail.com>
2019-02-24 00:49:23 -0600

    Add Home styling and work/play buttons

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,14 +6,34 @@ import styles from './Home.css';
 
 type Props = {};
 
+
+
 export default class Home extends Component<Props> {
+  //<div className="centered">
   props: Props;
 
   render() {
     return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+      <div>
+      <div className={styles.grad}>
+        <div className={styles.split+" "+styles.left}>
+          <div className={styles.centered}>
+            <div className={styles.button+" "+styles.buttonBlue}>
+              <h2>Work</h2>
+            </div>
+            <p>Click here to begin working</p>
+          </div>
+        </div>
+
+        <div className={styles.split+" "+styles.right}>
+          <div className={styles.centered}>
+            <div className={styles.button+" "+styles.buttonOrange}>
+              <h2>Play</h2>
+            </div>
+            <p>Click here to begin playing</p>
+          </div>
+        </div>
+      </div>
       </div>
     );
   }
