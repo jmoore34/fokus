8e0002b5cad5ed6af95e64bcd084556a1059993f
tjenator <tnnrsvrt@gmail.com>
2019-02-24 01:58:00 -0600

    implement timer

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Counter.css';
 import routes from '../constants/routes';
+import {remote} from 'electron';
+import format from "format-duration";
+
 
 type Props = {
   increment: () => void,
@@ -15,59 +18,32 @@ type Props = {
 export default class Counter extends Component<Props> {
   props: Props;
 
-  render() {
-    const {
-      increment,
-      incrementIfOdd,
-      incrementAsync,
-      decrement,
-      counter
-    } = this.props;
-    return (
-      <div>
-        <div className={styles.backButton} data-tid="backButton">
-          <Link to={routes.HOME}>
-            <i className="fa fa-arrow-left fa-3x" />
-          </Link>
-        </div>
-        <div className={`counter ${styles.counter}`} data-tid="counter">
-          {counter}
-        </div>
-        <div className={styles.btnGroup}>
-          <button
-            className={styles.btn}
-            onClick={increment}
-            data-tclass="btn"
-            type="button"
-          >
-            <i className="fa fa-plus" />
-          </button>
-          <button
-            className={styles.btn}
-            onClick={decrement}
-            data-tclass="btn"
-            type="button"
-          >
-            <i className="fa fa-minus" />
-          </button>
-          <button
-            className={styles.btn}
-            onClick={incrementIfOdd}
-            data-tclass="btn"
-            type="button"
-          >
-            odd
-          </button>
-          <button
-            className={styles.btn}
-            onClick={() => incrementAsync()}
-            data-tclass="btn"
-            type="button"
-          >
-            async
-          </button>
-        </div>
-      </div>
-    );
+  constructor(props){
+    super(props);
+    this.state = {remaining: 0}
   }
+
+  componentDidMount(){
+    this.interval = setInterval(
+      ()=> this.tick(), 1000
+    )
+  }
+  tick(){
+    const state = remote.getGlobal("getCurrentStatus")();
+    const startTime = state.startTime.valueOf();
+    this.setState({remaining: (startTime + state.duration) - Date.now()});
+    console.log(state.remaining, startTime, state.duration, ((startTime + state.duration) - Date.now()));
+
+  }
+  render() {
+
+    const state = remote.getGlobal("getCurrentStatus")();
+
+    return (
+    <div>
+      <div className = {styles.mockup}>Time Remaining: {format(this.state.remaining)}</div>
+      <div className = {styles.mockup}> Task: {state.name} ({state.play ? "play" : "focus"})</div>
+    </div>
+    )
+}
 }
