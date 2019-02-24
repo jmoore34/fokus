a1b814c8e7fb8a2edd27ea376919b9fd6b711fcb
jon <programjm@gmail.com>
2019-02-24 00:41:47 -0600

    Add change URL capability

--- app/Routes.js
+++ app/Routes.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router';
 import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
+import { remote } from 'electron';
 
-export default () => (
-  <App>
-    <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
-    </Switch>
-  </App>
-);
+
+export default class extends Component<Props> {
+
+
+  render() {
+    const timerMode = remote.getGlobal("getCurrentStatus")().timerMode;
+    return (
+      <App>
+          { timerMode ? <CounterPage/> :
+            <Switch>
+                <Route path={routes.HOME} component={HomePage}/>
+            </Switch>
+          }
+      </App>
+    );
+  }
+}
+
+
+
+
