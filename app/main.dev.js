1434001ed6f51b24c522e3647b92ebebb0740265
Jonathan Moore <programjm@gmail.com>
2020-07-17 16:57:47 -0500

    Disable tool menu

--- app/main.dev.js
+++ app/main.dev.js
@@ -165,8 +165,10 @@ app.on('ready', async () => {
     mainWindow = null;
   });
 
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
+  ////// Disabled //////
+  // const menuBuilder = new MenuBuilder(mainWindow);
+  // menuBuilder.buildMenu();
+
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
   new AppUpdater();
